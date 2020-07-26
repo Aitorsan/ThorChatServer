b638560645b854bce3c98bfee91a56dccb3fd29e
aitsan <aitor.sanmartin11@gmail.com>
2020-07-26 13:52:49 +0200

    Google test added for unit testing

---
+++ _Tests/test_main.cpp
@@ -0,0 +1,11 @@
+//Google test entry point
+#include <gtest/gtest.h>
+
+//NOTE: Include all tests under the gtest so we do not have to include it in every test
+#include "base64_test.h"
+
+int main(int argc, char* argv[])
+{
+	testing::InitGoogleTest(&argc,argv);
+	return RUN_ALL_TESTS();
+}
